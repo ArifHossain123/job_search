a0e029cbc5e1a854b6c0c1b614c8338750cd655e
Arif Hossain <alivearif120@gmail.com>
2024-03-19 21:24:50 +0100

    new page

--- lib/main.dart
+++ lib/main.dart
@@ -16,11 +16,33 @@ class JobSearch extends StatelessWidget {
     );
   }
 }
+
 class JobSearchAppBar extends StatelessWidget {
   const JobSearchAppBar({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('AppBar Separated from Main'),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.abc),
+          ),
+        ],
+      ),
+      body: Column(
+        children: [
+          TextButton(
+            onPressed: () {},
+            child: const Icon(
+              Icons.alarm,
+            ),
+          ),
+          //Checkbox(value: value, onChanged: onChanged)
+        ],
+      ),
+    );
   }
 }
