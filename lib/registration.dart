a17174c2a584ae0b4070b901a4dc4c03f0ecb638
Arif Hossain <alivearif120@gmail.com>
2024-03-22 22:35:36 +0100

    Clipper

--- lib/registration.dart
+++ lib/registration.dart
@@ -71,56 +71,88 @@ class _AppRegistrationState extends State<AppRegistration> {
               ),
               Column(
                 children: [
-                  Container(
-                    height: 130,
-                    child: Stack(
-                      children: [
-                        Positioned(
-                          left: 20,
-                          child: Material(
-                            child: Container(
-                              height: 130.0,
-                              width: width * 0.9,
-                              decoration: BoxDecoration(
-                                color: const Color.fromARGB(255, 243, 243, 243),
-                                borderRadius: BorderRadius.circular(10),
-                                boxShadow: [
-                                  BoxShadow(
-                                    color:
-                                        const Color.fromARGB(255, 201, 201, 201)
-                                            .withOpacity(.6),
-                                    spreadRadius: 1,
-                                    blurRadius: 3,
-                                    offset: const Offset(-3, 3),
-                                  ),
-                                ],
+                  InkWell(
+                    onTap: () {},
+                    child: Container(
+                      height: 130,
+                      child: Stack(
+                        children: [
+                          Positioned(
+                            left: 20,
+                            child: Material(
+                              child: Container(
+                                height: 130.0,
+                                width: width * 0.9,
+                                decoration: BoxDecoration(
+                                  color:
+                                      const Color.fromARGB(255, 243, 243, 243),
+                                  borderRadius: BorderRadius.circular(10),
+                                  boxShadow: [
+                                    BoxShadow(
+                                      color: const Color.fromARGB(
+                                              255, 201, 201, 201)
+                                          .withOpacity(.6),
+                                      spreadRadius: 1,
+                                      blurRadius: 3,
+                                      offset: const Offset(-3, 3),
+                                    ),
+                                  ],
+                                ),
                               ),
                             ),
                           ),
-                        ),
-                        Positioned(
-                          top: 0,
-                          left: 20,
-                          child: Container(
-                            child: ClipPath(
-                              clipper: CustomClipPath(),
-                              child: Container(
-                                height: 100,
-                                width: width * 0.25,
-                                decoration: const BoxDecoration(
-                                  color: Color.fromARGB(255, 14, 158, 19),
-                                  borderRadius: BorderRadius.only(
-                                    topLeft: Radius.circular(10),
-                                    topRight: Radius.circular(10),
-                                    bottomRight: Radius.circular(100),
-                                    bottomLeft: Radius.circular(10),
+                          Positioned(
+                            top: 0,
+                            left: 20,
+                            child: Container(
+                              child: ClipPath(
+                                clipper: CustomClipPath(),
+                                child: Container(
+                                  height: 100,
+                                  width: width * 0.25,
+                                  decoration: const BoxDecoration(
+                                    color: Color.fromARGB(255, 14, 158, 19),
+                                    borderRadius: BorderRadius.only(
+                                      topLeft: Radius.circular(10),
+                                      topRight: Radius.circular(10),
+                                      bottomRight: Radius.circular(100),
+                                      bottomLeft: Radius.circular(10),
+                                    ),
                                   ),
                                 ),
                               ),
                             ),
                           ),
-                        ),
-                      ],
+                          Positioned(
+                            top: 20,
+                            left: 125,
+                            child: Column(
+                              children: [
+                                Container(
+                                  child: const Text(
+                                    "Special Skilled Category",
+                                    style: TextStyle(
+                                      color: Color.fromARGB(255, 0, 66, 188),
+                                      fontSize: 23,
+                                    ),
+                                  ),
+                                ),
+                                const SizedBox(
+                                  height: 15,
+                                ),
+                                Container(
+                                  child: const Text(
+                                    "Electronics Tecnition, Driver,",
+                                    style: TextStyle(
+                                      fontSize: 19,
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
                     ),
                   ),
                   const SizedBox(
@@ -195,15 +227,13 @@ class _AppRegistrationState extends State<AppRegistration> {
                                 const SizedBox(
                                   height: 15,
                                 ),
-                                Stack(
-                                  children: [
-                                    const Text(
-                                      "Electronics Tecnition, Driver,",
-                                      style: TextStyle(
-                                        fontSize: 19,
-                                      ),
+                                Container(
+                                  child: const Text(
+                                    "Electronics Tecnition, Driver,",
+                                    style: TextStyle(
+                                      fontSize: 19,
                                     ),
-                                  ],
+                                  ),
                                 ),
                               ],
                             ),
