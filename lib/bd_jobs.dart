a584b750a1a927279e7ebf63ca941533061b4562
Arif Hossain <alivearif120@gmail.com>
2024-03-23 19:13:39 +0100

    Select skill

--- lib/bd_jobs.dart
+++ lib/bd_jobs.dart
@@ -11,24 +11,72 @@ class Jobs extends StatefulWidget {
 class _JobsState extends State<Jobs> {
   @override
   Widget build(BuildContext context) {
+    // final double height = MediaQuery.of(context).size.height;
+    final double width = MediaQuery.of(context).size.width;
     return Scaffold(
       appBar: AppBar(
-        actions: [
-          TextButton(
-            onPressed: () {},
-            child: Container(
-              height: 30,
-              width: 30,
-              color: Colors.red,
-              child: Text('lcmxskm'),
-            ),
+        actions: [],
+      ),
+      body: ListView(
+        children: [
+          Column(
+            children: [
+              Column(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(
+                      left: 20,
+                    ),
+                    child: Expanded(
+                      child: Container(
+                        height: 8,
+                        width: 280,
+                        decoration: BoxDecoration(
+                          color: Color.fromARGB(255, 204, 204, 204),
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        child: Row(
+                          children: [
+                            Container(
+                              height: 8,
+                              width: width * 0.2,
+                              decoration: BoxDecoration(
+                                color: Colors.green,
+                                borderRadius: BorderRadius.circular(10),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                  const Padding(
+                    padding: EdgeInsets.only(left: 20),
+                    child: SizedBox(
+                      height: 60,
+                      child: Row(
+                        children: [
+                          Text(
+                            'Select Skills from the list',
+                            style: TextStyle(fontSize: 18),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ],
           ),
         ],
       ),
       floatingActionButton: FloatingActionButton(
-        child: const Icon(
-          Icons.arrow_forward,
-          color: Colors.white,
+        backgroundColor: Colors.green,
+        child: Container(
+          child: const Icon(
+            Icons.arrow_forward,
+            color: Colors.white,
+          ),
         ),
         onPressed: () {
           Navigator.push(
