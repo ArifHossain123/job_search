31e502aeeb0837b134a6226f73a91946de2e938a
Arif Hossain <alivearif120@gmail.com>
2024-03-25 10:25:12 +0100

    TextFormField

--- lib/enter_information.dart
+++ lib/enter_information.dart
@@ -15,42 +15,74 @@ class _EnterYourInformationState extends State<EnterYourInformation> {
     final double width = MediaQuery.of(context).size.width;
     return Scaffold(
       appBar: AppBar(
-        actions: [
-          
-        ],
+        actions: [],
       ),
-      body: Column(
+      body: ListView(
         children: [
           Column(
             children: [
               Column(
                 children: [
-                  Padding(
-                    padding: const EdgeInsets.only(top: 30,
-                      left: 40,
-                    ),
-                    child: Expanded(
-                      child: Container(
-                        height: 8,
-                        width: 300,
-                        decoration: BoxDecoration(
-                          color: const Color.fromARGB(255, 204, 204, 204),
-                          borderRadius: BorderRadius.circular(10),
+                  Column(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(
+                          top: 30,
+                          left: 20,
                         ),
-                        child: Row(
-                          children: [
-                            Container(
-                              height: 8,
-                              width: width * 0.4,
-                              decoration: BoxDecoration(
-                                color: Colors.green,
-                                borderRadius: BorderRadius.circular(10),
-                              ),
+                        child: Expanded(
+                          child: Container(
+                            height: 8,
+                            width: 300,
+                            decoration: BoxDecoration(
+                              color: const Color.fromARGB(255, 204, 204, 204),
+                              borderRadius: BorderRadius.circular(10),
                             ),
-                          ],
+                            child: Row(
+                              children: [
+                                Container(
+                                  height: 8,
+                                  width: width * 0.4,
+                                  decoration: BoxDecoration(
+                                    color: Colors.green,
+                                    borderRadius: BorderRadius.circular(10),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
                         ),
                       ),
-                    ),
+                      //
+                      const Row(
+                        children: [
+                          Padding(
+                            padding: EdgeInsets.only(
+                              left: 30,
+                              top: 40,
+                            ),
+                            child: Text(
+                              'Write Your Full Name',
+                              style: TextStyle(
+                                fontSize: 22,
+                              ),
+                            ),
+                          ),
+                          SizedBox(
+                            height: 20,
+                          ),
+                        ],
+                      ),
+                      TextFormField(
+                        decoration: InputDecoration(
+                          labelText: 'Write Your Name',
+                          icon: Icon(
+                            Icons.verified_user,
+                          ),
+                          border: OutlineInputBorder(),
+                        ),
+                      ),
+                    ],
                   ),
                 ],
               ),
