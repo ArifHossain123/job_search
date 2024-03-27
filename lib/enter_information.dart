7cb3308404b700bd41250961416e0579175f7ead
Arif Hossain <alivearif120@gmail.com>
2024-03-27 23:58:18 +0100

    TextField

--- lib/enter_information.dart
+++ lib/enter_information.dart
@@ -11,7 +11,7 @@ class EnterYourInformation extends StatefulWidget {
 class _EnterYourInformationState extends State<EnterYourInformation> {
   @override
   Widget build(BuildContext context) {
-    //final double height = MediaQuery.of(context).size.height;
+    final double height = MediaQuery.of(context).size.height;
     final double width = MediaQuery.of(context).size.width;
     return Scaffold(
       appBar: AppBar(
@@ -90,6 +90,80 @@ class _EnterYourInformationState extends State<EnterYourInformation> {
                           ),
                         ),
                       ),
+                            const SizedBox(
+                        height: 10,
+                      ),
+                      Container(
+                        color: const Color.fromARGB(255, 254, 251, 251),
+                        height: height * 0.24,
+                        width: width * 0.94,
+                        child: Padding(
+                          padding: const EdgeInsets.only(
+                            left: 110,
+                          ),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            mainAxisAlignment: MainAxisAlignment.end,
+                            children: <Widget>[
+                              const Row(
+                                children: [
+                                  Text(
+                                    '   For any help',
+                                    style: TextStyle(
+                                      color: Colors.black,
+                                      fontSize: 18,
+                                    ),
+                                  )
+                                ],
+                              ),
+                              const SizedBox(
+                                height: 4,
+                              ),
+                              Row(
+                                children: <Widget>[
+                                  RichText(
+                                    text: TextSpan(
+                                      children: [
+                                        const TextSpan(
+                                          text: 'Call at   ',
+                                          style: TextStyle(
+                                            color: Colors.green,
+                                            fontSize: 18,
+                                          ),
+                                        ),
+                                        WidgetSpan(
+                                          child: Container(
+                                            height: 20,
+                                            width: 20,
+                                            decoration: BoxDecoration(
+                                              color: Colors.green,
+                                              borderRadius:
+                                                  BorderRadius.circular(5),
+                                            ),
+                                            child: const Icon(
+                                              Icons.call,
+                                              size: 14,
+                                              color: Colors.white,
+                                            ),
+                                          ),
+                                        ),
+                                        const TextSpan(
+                                          text: '  10101',
+                                          style: TextStyle(
+                                            color: Colors.green,
+                                            fontSize: 18,
+                                          ),
+                                        )
+                                      ],
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                   
                     ],
                   ),
                 ],
